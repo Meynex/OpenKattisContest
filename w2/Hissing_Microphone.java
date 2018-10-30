e4ba8612a8f5193d77626331c91f8f7dcaed45ed
Meynex <30909071+Meynex@users.noreply.github.com>
2018-10-30 08:48:36 +0100

    Create Hissing_Microphone.java

---
+++ w2/Hissing_Microphone.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Hissing_Microphone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner Sc = new Scanner(System.in);
+		
+		String input = Sc.nextLine();
+		if(input.contains("ss")) System.out.println("hiss");
+		else System.out.println("no hiss");
+	}
+
+}
